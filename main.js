065eaf969fac9f44bbe0e0b0d518006b368e7034
“Allie <allieroll22@ufl.edu>
2019-03-01 12:19:34 -0500

    Add classes to HTML

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// It is always helpful to use comments in your code!
+var mainMenu = document.getElementById("dropdown1");
+console.log(mainMenu);
+/* function myFunction() {
+  this.nextElementSibling.classList.toggle('show');
+};
+
+for (i=0; mainMenu.length; i++) {
+  mainMenu[i].addEventListener('mouseenter', myFunction);
+} */
