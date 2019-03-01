76b2abfd049ef11f27d035e5b8436ba84d5dd83d
“Allie <allieroll22@ufl.edu>
2019-03-01 15:00:49 -0500

    Add CSS transition

--- main.js
+++
@@ -1,9 +0,0 @@
-var mainMenu = document.getElementById("dropdown1");
-console.log(mainMenu);
-/* function myFunction() {
-  this.nextElementSibling.classList.toggle('show');
-};
-
-for (i=0; mainMenu.length; i++) {
-  mainMenu[i].addEventListener('mouseenter', myFunction);
-} */
